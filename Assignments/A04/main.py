a2d945fb2f3ae238c8255b3860cfc7d4761adbe6
mikamorgan <mmorgan@archercityisd.net>
2020-07-10 14:05:50 -0500

    Correct alignment

--- Assignments/A04/main.py
+++ Assignments/A04/main.py
@@ -2,32 +2,45 @@
 
 import sys, json, os
 import tkinter as tk
+from tkinter.font import Font
 
 # GUI window is a subclass of the basic tkinter Frame object
 class PlayerInfoFrame(tk.Frame):
     def __init__(self, master,player_dictionary):
+        # Choose your font
+        #appfont = font.Font(family='Courier New', size=12, weight='bold')
+        
         # Call superclass constructor
         tk.Frame.__init__(self, master)
+        
         # Place frame into main window
         self.grid()
 
         # Create text box with player information
         row = 0
-        col = 0
+        col = 1
 
         for key in player_dictionary:
-            pad = " "* (30 - len(key))
 
             if type(player_dictionary[key]) is list:
-                lab = tk.Label(self, text=f"{key}{pad}:")
-                lab.grid(row=row, column=col)
+                front = tk.Label(self, text=f"{key}")
+                front.grid(row=row, column=0, sticky=tk.W)
+
+                mid   = tk.Label(self, text=f":")
+                mid.grid(row=row, column=1)
+
                 for value in player_dictionary[key]:
                     col += 1
-                    lab = tk.Label(self, text=f"{value}, ")
-                    lab.grid(row=row, column=col)
+                    back = tk.Label(self, text=f"{value},")
+                    back.grid(row=row, column=col, sticky=tk.W)
             else:
-                lab = tk.Label(self, text=f"{key}{pad}:\t{player_dictionary[key]}")
-                lab.grid(row=row, column=col)
+                front = tk.Label(self, text=f"{key}")
+                mid   = tk.Label(self, text=f":")
+                back  = tk.Label(self, text=f"{player_dictionary[key]}")
+                
+                front.grid(row=row, column=0, sticky=tk.W)
+                mid.grid(row=row, column=1)
+                back.grid(row=row, column=2, sticky=tk.W)
 
             row += 1
 
