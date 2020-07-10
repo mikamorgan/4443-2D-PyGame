7031fa4650df4038b0233075af05abdea3415e35
mikamorgan <mmorgan@archercityisd.net>
2020-07-10 09:22:13 -0500

    Parse json file into Python dictionary

--- Assignments/A04/main.py
+++ Assignments/A04/main.py
@@ -1,51 +1,39 @@
+#!/usr/bin/env python
+
+import sys, json, os
 import tkinter as tk
-import sys
-import os
-import json
 
 # GUI window is a subclass of the basic tkinter Frame object
 class PlayerInfoFrame(tk.Frame):
-    def __init__(self, master,data):
+    def __init__(self, master,player_dictionary):
         # Call superclass constructor
         tk.Frame.__init__(self, master)
         # Place frame into main window
         self.grid()
 
         # Create text box with player information
-        fName = tk.Label(self, text=f"Hello ")
-        fName.grid(row=0, column=0)
+        row = 0
 
-        lName = tk.Label(self, text=f"Hello ")
-        lName.grid(row=1, column=0)
+        for info in player_dictionary:
+            lab = tk.Label(self, text=f"{info} \t: {info}")
+            lab.grid(row=row, column=0)
 
-        rank = tk.Label(self, text=f"Hello")
-        rank.grid(row=2, column=0)
-
-        speed = tk.Label(self, text=f"Hello ")
-        speed.grid(row=3, column=0)
-
-        power = tk.Label(self, text=f"Hello")
-        power.grid(row=2, column=0)
-
-        health = tk.Label(self, text=f"Hello ")
-        health.grid(row=3, column=0)
+            row += 1
 
 # Spawn window
 if __name__ == "__main__":
-    # Open json file with player information
-    fpath = sys.argv[0]
+    # Open json file with player information (get file name as command line paramater)
+    with open(sys.argv[1], 'r') as f:
+        data = f.read()
+        f.close()
 
-    f = open(fpath,"r")
-    data = f.read()
-    f.close()
-
-    player = json.loads(data)
+        player_dictionary = json.loads(data)
 
     # Create main window object
     root = tk.Tk()
     # Set title of window
     root.title("Player: ")
-    # Instantiate HelloWorldFrame object
-    player_frame = PlayerInfoFrame(root, data)
-    # Start GUI
+    # Instantiate PlayerInfoFrame object, pass data dictionary
+    player_frame = PlayerInfoFrame(root, player_dictionary)
+    # Start GUI 
     player_frame.mainloop()
