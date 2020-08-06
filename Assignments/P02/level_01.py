63fe51b13be5d2b72b36b47b11c6c5743958aeb5
mikamorgan <mmorgan@archercityisd.net>
2020-08-05 22:14:01 -0500

    Add P02.0 (Copy of Griffin program)

--- Assignments/P02/level_01.py
+++ Assignments/P02/level_01.py
@@ -1,31 +1,130 @@
-import sys
 import pygame as pg
+import random
+import sys
+from random import choice
+from settings import *
+from os import path
 from state import State
+import sprites
+from sprites import Spritesheet
 
 class Level_01(State):
     def __init__(self):
         super(Level_01, self).__init__()
-        self.rect = pg.Rect((0, 0), (128, 128))
-        self.x_velocity = 1
-        
-    def startup(self, persistent):
-        self.persist = persistent
+        # initialize game window, etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
+        self.load_data()
 
-        
-    def get_event(self, event):
-        if event.type == pg.QUIT:
-            self.quit = True
-        elif event.type == pg.MOUSEBUTTONUP:
-            pass
-        
-    def update(self, dt):
-        self.rect.move_ip(self.x_velocity, 0)
-        if (self.rect.right > self.screen_rect.right
-            or self.rect.left < self.screen_rect.left):
-            self.x_velocity *= -1
-            self.rect.clamp_ip(self.screen_rect)
-                 
-    def draw(self, surface):
-        #surface.fill(self.screen_color)
-        #surface.blit(self.title, self.title_rect)
-        pg.draw.rect(surface, pg.Color("darkgreen"), self.rect)
+    def load_data(self):
+        # load high score
+        self.dir = path.dirname(__file__)
+        img_dir = path.join(self.dir, 'img')
+        with open(path.join(self.dir, HS_FILE), 'r') as f:
+            try:
+                self.highscore = int(f.read())
+            except:
+                self.highscore = 0
+        # load spritesheet image
+        self.spritesheet = sprites.Spritesheet(path.join(img_dir, SPRITESHEET))
+
+    def new(self):
+        # start a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(self, *plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        self.run()
+
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+        # check if player hits a platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+
+        # if player reaches top 1/4 of screen
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += max(abs(self.player.vel.y), 2)
+            for plat in self.platforms:
+                plat.rect.y += max(abs(self.player.vel.y), 2)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 10
+
+        # Die!
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.all_sprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False
+
+        # spawn new platforms to keep same average number
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            p = Platform(self, random.randrange(0, WIDTH - width),
+                         random.randrange(-75, -30))
+            self.platforms.add(p)
+            self.all_sprites.add(p)
+
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BGCOLOR)
+        self.all_sprites.draw(self.screen)
+        self.screen.blit(self.player.image, self.player.rect)
+        pg.display.flip()
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
+
+
+g = Level_01()
+
+while g.running:
+    g.new()
+
+pg.quit()
